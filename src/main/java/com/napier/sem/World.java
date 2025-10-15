8006274a9ebe08c9d25e7023ec2bbf4921fcb950
patrykizdebski01 <40722089@live.napier.ac.uk>
2025-10-15 09:29:35 +0100

    Added some comments

--- src/main/java/com/napier/sem/World.java
+++ src/main/java/com/napier/sem/World.java
@@ -1,10 +1,12 @@
 package com.napier.sem;
 
 /**
- * Represents an employee
+ * Represents the country table in the world database
  */
 public class World
-{
+{   /**
+    * Initializing properties for different columns
+    */
 
     public int Code;
 
